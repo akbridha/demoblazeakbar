3d43a8ed39542ad1ae98ff0b4eb9cbb1dcf68605
akbarKehidupan <akbridha@gmail.com>
2026-01-20 16:46:19 +0800

    modif end to end single category

--- Scripts/scenario/end_to_end_checkout_use_filter_single_category/Script1767166903473.groovy
+++ Scripts/scenario/end_to_end_checkout_use_filter_single_category/Script1767166903473.groovy
@@ -21,22 +21,30 @@ def products = ['Iphone 6 32gb', 'Nexus 6', 'Samsung galaxy s7']
 
 def tableHeader = ['Pic', 'Title', 'Price']
 
-def availableCategory = ["Phones", "Laptops", "Monitors"]
+def availableCategory = ['Phones', 'Laptops', 'Monitors']
 
-WebUI.openBrowser("https://www.demoblaze.com/index.html")
+WebUI.openBrowser('https://www.demoblaze.com/index.html')
+
 for (String c : availableCategory) {
-    WebUI.callTestCase(findTestCase('page/home/button_filter_category_match'), [('categoryName'): c], FailureHandling.STOP_ON_FAILURE)
-    WebUI.navigateToUrl("https://www.demoblaze.com/index.html")
+    WebUI.callTestCase(findTestCase('page/home/button_filter_category_match'), [('categoryName') : c], FailureHandling.STOP_ON_FAILURE)
+
+    WebUI.navigateToUrl('https://www.demoblaze.com/index.html')
 }
 
 //WebUI.callTestCase(findTestCase('page/home/button_filter_category_match'), [:], FailureHandling.STOP_ON_FAILURE)
-
 for (int i = 0; i < products.size(); i++) {
-	String p = products[i]
-	WebUI.callTestCase(findTestCase('page/product/product_detail_match'), [('productName'): p], FailureHandling.STOP_ON_FAILURE)
-	WebUI.callTestCase(findTestCase('page/product/product_add_to_cart_dynamic_single'), [('productName'): p], FailureHandling.STOP_ON_FAILURE)
-	WebUI.navigateToUrl('https://www.demoblaze.com/index.html')
+    String p = products[i]
+
+    WebUI.callTestCase(findTestCase('page/product/product_detail_match'), [('productName') : p], FailureHandling.STOP_ON_FAILURE)
+
+    WebUI.callTestCase(findTestCase('page/product/product_add_to_cart_dynamic_single'), [('productName') : p], FailureHandling.STOP_ON_FAILURE)
+
+    WebUI.navigateToUrl('https://www.demoblaze.com/index.html')
 }
 
 WebUI.callTestCase(findTestCase('page/cart/cart_page_opened'), [('arrayPassedTableHeader') : tableHeader], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.callTestCase(findTestCase('page/cart/place_order'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.callTestCase(findTestCase('page/cart/order_info_match'), [:], FailureHandling.STOP_ON_FAILURE)
 
